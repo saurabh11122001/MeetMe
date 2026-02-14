27ed3d7059a58036ae7b837a91a5f0a74e1cec12
Saurabh Singh Rautela <saurabhrautela2001@gmail.com>
2026-02-14 19:02:16 +0530

    Update cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config({});
 
 cloudinary.config({
-    cloud_name: "dp4ljnoq5",
-    api_key: "288514684828149",
-    api_secret: "BDb5wgQ6Z-UFwjdtJ6Y94lwfNCo"
+    cloud_name: "",
+    api_key: "",
+    api_secret: ""
 });
 export default cloudinary;
